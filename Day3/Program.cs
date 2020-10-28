c37a60c32dc2b8780ee3a4ec2b501ab8da87ff5e
Kristaps Krumins <kristaps.krumins@tieto.com>
2020-10-28 15:21:46 +0200

    Day3 dev

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -11,9 +11,58 @@ namespace Day3
         }
         static void Task2()
         {
-            // Convert.ToBoolean
-            // bool.Parse
+            // bool.Parse izmetīs kļūdu, ja nespēs konvērtēt string vērtību
+            // piemēram, ievadot jebko, kas nav ne true, ne false - būs kļūda!
+            // tāpēc tika izmantots try/catch bloks, lai šādu kļūdu noķertu
+            // Alternatīva - izmantot bool.TryParse
+            while(true)
+            {
+                Console.WriteLine("Vai Tu vēlies pusdienas? (TRUE/FALSE)");
+                string input = Console.ReadLine();
+                try
+                {
+                    if (bool.Parse(input))
+                    {
+                        Console.WriteLine("Jūsu pusdienas ir gatavas!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Jums pusdienas nebūs...");
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Nesapratu...");
+                }
+            }
+        }
+        static void Task3()
+        {
+            bool myBoolean = true;
+            //ar while
+            int i = 0;
+            while (myBoolean)
+            {
+                if (++i == 20)
+                    myBoolean = false;
+            }
 
+            // ar do/while
+            /*int i = 0;
+            do
+            {
+                if (++i == 20)
+                    myBoolean = false;
+            } while (myBoolean);*/
+
+            // Ar for
+            /*for(int i = 1; myBoolean; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 20)
+                    myBoolean = false;
+            }*/
         }
         static void Main(string[] args)
         {
@@ -26,7 +75,10 @@ namespace Day3
                     Task1();
                     break;
                 case "2":
-                    //Task2();
+                    Task2();
+                    break;
+                case "3":
+                    Task3();
                     break;
                 case "exit":
                     return;

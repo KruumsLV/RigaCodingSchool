e1b63ddac85a40d4216670052c662c67ae625f45
Kristaps Krumins <kristaps.krumins@tieto.com>
2020-10-29 19:07:52 +0200

    Example commit

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day4
 {
@@ -6,7 +7,7 @@ namespace Day4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
